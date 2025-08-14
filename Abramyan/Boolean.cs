00c2d112fb39bdecab1f3804efc39b54d9a6ab58
Svyatoslav-Vtulkin <vss200601@gmail.com>
2025-08-14 20:20:25 +0500

    add and config 22-28,30,34-37 Boolean

--- Abramyan/Boolean.cs
+++ Abramyan/Boolean.cs
@@ -174,8 +174,134 @@
 // Boolean20 Дано трехзначное число. Проверить истинность высказывания: «Все цифры данного числа различны».
 // Console.Write("Введите значение A: ");
 // int a = int.Parse(Console.ReadLine()!);
-// Console.Write("Введите значение B: ");
 // bool isDifferent = a % 10 != a / 10 % 10 && a / 10 % 10 != a / 100 && a % 10 != a / 100;
 // Console.WriteLine(isDifferent);
 
-//
+// Boolean21 Дано трехзначное число.
+// Проверить истинность высказывания:«Цифры данного числа образуют возрастающую последовательность».
+// Console.Write("Введите значение А: ");
+// int a = int.Parse(Console.ReadLine()!);
+// int a1 = a / 100;
+// int a2 = a / 10 % 10;
+// int a3 = a % 10;
+// bool isIncreasingSequence = a1 < a2 && a1 < a3 && a2 < a3 ;
+// Console.WriteLine(isIncreasingSequence);
+
+// Boolean22 Дано трехзначное число.
+// Проверить истинность высказывания: «Цифры данного числа образуют возрастающую или убывающую последовательность».
+// Console.Write("Введите значение А: ");
+// int a = int.Parse(Console.ReadLine()!);
+// int a1 = a / 100;
+// int a2 = a / 10 % 10;
+// int a3 = a % 10;
+// bool isSequence = (a1 < a2 && a1 < a3 && a2 < a3) || (a1 > a2 && a1 > a3 && a2 > a3);
+// Console.WriteLine(isSequence);
+
+// Boolean23 Дано четырехзначное число.
+// Проверить истинность высказывания: «Данное число читается одинаково слева направо и справа налево».
+// Console.Write("Введите значение А: ");
+// int a = int.Parse(Console.ReadLine()!);
+// int a1 = a / 1000;
+// int a2 = a % 1000 / 100;
+// int a3 = a / 10 % 10;
+// int a4 = a % 10;
+// bool isSequence = a1 == a4 && a2 == a3;
+// Console.WriteLine(isSequence);
+
+// Boolean25 Даны числа x, y.
+// Проверить истинность высказывания: «Точка с координатами (x, y) лежит во второй координатной четверти».
+// Console.Write("Введите значение X: ");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение Y: ");
+// int y = int.Parse(Console.ReadLine()!);
+// bool isInSecondQuarter = x < 0 && y > 0;
+// Console.WriteLine(isInSecondQuarter);
+
+// Boolean26 Даны числа x, y.
+// Проверить истинность высказывания: «Точка с координатами (x, y) лежит в четвертой координатной четверти»
+// Console.Write("Введите значение X: ");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение Y: ");
+// int y = int.Parse(Console.ReadLine()!);
+// bool isInFourQuarter = x > 0 && y < 0;
+// Console.WriteLine(isInFourQuarter);
+
+// Boolean27 Даны числа x, y.
+// Проверить истинность высказывания: «Точка с координатами (x, y) лежит во второй или третьей координатной четверти».
+// Console.Write("Введите значение X: ");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение Y: ");
+// int y = int.Parse(Console.ReadLine()!);
+// bool isInQuarter = (x < 0 && y > 0) || (x < 0 && y < 0);
+// Console.WriteLine(isInQuarter);
+
+// Boolean28 Даны числа x, y.
+// Проверить истинность высказывания: «Точка с координатами (x, y) лежит в первой или третьей координатной четверти».
+// Console.Write("Введите значение X: ");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение Y: ");
+// int y = int.Parse(Console.ReadLine()!);
+// bool isInQuarter = (x > 0 && y > 0) || (x < 0 && y < 0);
+// Console.WriteLine(isInQuarter);
+
+// Boolean30 Даны целые числа a, b, c, являющиеся сторонами некоторого треугольника.
+// Проверить истинность высказывания: «Треугольник со сторонами a, b, c является равносторонним».
+// Console.Write("Введите значение A: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение B: ");
+// int b = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение C: ");
+// int c = int.Parse(Console.ReadLine()!);
+// bool isEquilateralTriangle = a == b && b == c;
+// Console.WriteLine(isEquilateralTriangle);
+
+// Boolean34 Даны координаты поля шахматной доски x, y (целые числа, лежащие в диапазоне 1–8).
+// Учитывая, что левое нижнее поле доски (1, 1)
+// является черным, проверить истинность высказывания: «Данное поле является белым»
+// Console.Write("Введите значение X: ");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение Y: ");
+// int y = int.Parse(Console.ReadLine()!);
+// bool isFieldWhite = (x + y) % 2 != 0;
+// Console.WriteLine(isFieldWhite);
+
+// Boolean35 Даны координаты двух различных полей шахматной доски x1,y1, x2, y2
+// (целые числа, лежащие в диапазоне 1–8). Проверить истинность высказывания: «Данные поля имеют одинаковый цвет».
+// Console.Write("Введите значение X1: ");
+// int x1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение Y1: ");
+// int y1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение X2: ");
+// int x2 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение Y2: ");
+// int y2 = int.Parse(Console.ReadLine()!);
+// bool haveSaveColor = (x1 + y1) % 2 == (x2 + y2) % 2;
+// Console.WriteLine(haveSaveColor);
+
+// Boolean36 Даны координаты двух различных полей шахматной доски x1, y1,x2, y2
+// (целые числа, лежащие в диапазоне 1–8). Проверить истинность высказывания:
+// «Ладья за один ход может перейти с одного поля на другое».
+// Console.Write("Введите значение X1: ");
+// int x1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение Y1: ");
+// int y1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение X2: ");
+// int x2 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение Y2: ");
+// int y2 = int.Parse(Console.ReadLine()!);
+// bool canRookMove = x1 == x2 || y1 == y2;
+// Console.WriteLine(canRookMove);
+
+// Boolean37 Даны координаты двух различных полей шахматной доски x1,y1, x2, y2
+// (целые числа, лежащие в диапазоне 1–8). Проверить истинность высказывания:
+// «Король за один ход может перейти с одного поля на другое».
+// Console.Write("Введите значение X1: ");
+// int x1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение Y1: ");
+// int y1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение X2: ");
+// int x2 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение Y2: ");
+// int y2 = int.Parse(Console.ReadLine()!);
+// bool canKingMove = Math.Abs(x1 - x2) < 2 && Math.Abs(y1 - y2) < 2;
+// Console.WriteLine(canKingMove);
